87fc4d298ad8b94ea325d879c3255a2d344958be
atikxcode <liadbd20@gmail.com>
2024-02-16 00:04:16 +0600

    finished the whole project

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,10 +1,62 @@
 function play(){
   hideElementById('home-screen');
+  hideElementById('final-score');
   showElementById('play-ground');
+
+  setTextElementValueById('current-life', 5);
+  setTextElementValueById('current-score', 0);
   continueGame();
+}
+
+function gameOver(){
+  hideElementById('play-ground');
+  showElementById('final-score');
+  const lastScore = getTextElementValueById('current-score');
+  console.log(lastScore);
+  setTextElementValueById('last-score', lastScore);
+  const currentAlphabet = getElementTextById('current-alphabet');
+  removeBackgroundColorById(currentAlphabet);
 }
 
 function continueGame(){
   const alphabet = getARandomAlphabet();
-  console.log('Your random alphabet', alphabet);
+  const currentAlphabetElement = document.getElementById('current-alphabet');
+  currentAlphabetElement.innerText = alphabet;
+  setBackgroundColorById(alphabet);
+
 }
+
+function handleKeyBoardKeyUpEvent(event){
+  const playerPressed = event.key;
+  console.log('player pressed', playerPressed);
+  if(playerPressed === 'Escape'){
+    gameOver();
+  }
+
+  const currentAlphabetElement = document.getElementById('current-alphabet');
+  const currentAlphabet = currentAlphabetElement.innerText;
+  const expectedAlphabet = currentAlphabet.toLowerCase();
+  console.log(playerPressed, expectedAlphabet);
+
+
+  if(playerPressed === expectedAlphabet){
+
+      const currentScore = getTextElementValueById('current-score');
+      const updatedScore = currentScore + 1;
+      setTextElementValueById('current-score', updatedScore);
+      removeBackgroundColorById(expectedAlphabet);
+      continueGame();
+  } else{
+
+    const currentLife = getTextElementValueById('current-life');
+    const updatedLife = currentLife - 1;
+    
+    setTextElementValueById('current-life', updatedLife);
+    if(updatedLife === 0){
+     gameOver();
+    }
+  }
+
+}
+
+ document.addEventListener('keyup', handleKeyBoardKeyUpEvent);
--- scripts/utility.js
+++ scripts/utility.js
@@ -7,6 +7,33 @@ function showElementById(elementId){
   element.classList.remove('hidden');
 }
 
+function setBackgroundColorById(elementId){
+  const element = document.getElementById(elementId);
+  element.classList.add('bg-[#FFA500]');
+}
+function removeBackgroundColorById(elementId){
+  const element = document.getElementById(elementId);
+  element.classList.remove('bg-[#FFA500]');
+}
+
+function getTextElementValueById(elementId){
+  const element = document.getElementById(elementId);
+  const elementValueText = element.innerText;
+  const value = parseInt(elementValueText);
+  return value;
+}
+
+function setTextElementValueById(elementId, value){
+  const element = document.getElementById(elementId);
+  element.innerText = value;
+}
+
+function getElementTextById(elementId){
+  const element = document.getElementById(elementId);
+  const text = element.innerText;
+  return text;
+}
+
 function getARandomAlphabet(){
   const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
   const alphabets = alphabetString.split('');
